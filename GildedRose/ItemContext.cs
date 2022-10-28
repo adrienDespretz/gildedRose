c6b7838feb26726e4c5916c57a0d078faa5e605b
Amieva <enzo.amieva@gmail.com>
2022-10-28 10:08:10 +0200

    Add bdd

---
+++ GildedRose/ItemContext.cs
@@ -0,0 +1,16 @@
+﻿using GildedRose.Items;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace GildedRose
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
--- GildedRose/itemsRepository/ItemsRepository.cs
+++ GildedRose/itemsRepository/ItemsRepository.cs
@@ -1,20 +1,30 @@
 using System.Collections.Generic;
+using System.Linq;
 using GildedRose.Items;
 
 namespace GildedRose.ItemsRepository
 {
     public abstract class ItemsRepository
     {
+        private readonly ItemContext _context;
+
+        public ItemsRepository(ItemContext context)
+        {
+            _context = context;
+        }
+
         public List<Item> itemList = new List<Item>();
         
         protected List<Item> GetInventory(){
-            return this.itemList;
+            return _context.Items.ToList();
         }
 
         protected void SaveInventory(List<Item> itemList){
             foreach(Item item in itemList){
-                this.itemList.Add(item);
+                _context.Items.Add(item);
             }
+            _context.SaveChanges();
+
         }
     }
 }
