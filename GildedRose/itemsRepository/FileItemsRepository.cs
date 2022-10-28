b8689884c76bc6c084b02cfd5a44e6db530b98b2
Amieva <enzo.amieva@gmail.com>
2022-10-28 09:36:29 +0200

    move fichier

--- GildedRose/itemsRepository/FileItemsRepository.cs
+++ GildedRose/itemsRepository/FileItemsRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GildedRose.items
+namespace GildedRose.itemsRepository
 {
-    public class InMemoryItemsRepository
+    public class FileItemsRepository
     {
     }
 }
