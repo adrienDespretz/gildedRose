0d1de92f21fabbc440d889d8cbe2969623539c67
adrienDespretz <adriendespretz40@gmail.com>
2022-11-15 09:48:37 +0100

    update

--- GildedRose/Repository/FileItemsRepository.cs
+++ GildedRose/Repository/FileItemsRepository.cs
@@ -1,4 +1,5 @@
 ﻿using GildedRose.Items;
+using GildedRose.ItemsRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,34 @@ using System.Threading.Tasks;
 
 namespace GildedRose.itemsRepository
 {
-    public class FileItemsRepository
+    public class FileItemsRepository : ItemRepository
     {
+        private readonly ItemContext _context;
 
+        public FileItemsRepository(ItemContext context)
+        {
+            _context = context;
+        }
+
+        public List<Item> itemList = new List<Item>();
+
+        void ItemRepository.SaveInventory(List<Item> itemList)
+        {
+            foreach (Item item in itemList)
+            {
+                _context.Items.Add(item);
+            }
+            _context.SaveChanges();
+        }
+
+        List<Item> ItemRepository.GetInventory()
+        {
+            return _context.Items.ToList();
+        }
+
+        Item ItemRepository.FindItem(string type, int quality)
+        {
+            return _context.Items.ToList().Where(x => x.quality == quality && x.name == type).FirstOrDefault();
+        }
     }
 }
