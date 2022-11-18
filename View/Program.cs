82d2f5381c2a65151dc04cf57574e097cffbfa67
adrienDespretz <adriendespretz40@gmail.com>
2022-11-18 10:18:16 +0100

    update affichage console

--- View/Program.cs
+++ View/Program.cs
@@ -5,42 +5,42 @@ namespace GildedRose {
 class Program
 {
         static void Main(string[] args)
-    {
+        {
             FileItemsRepository fileItemsRepository = new FileItemsRepository();
 
             Console.WriteLine("Gilded Rose\r");
-    Console.WriteLine("------------------------\n");
+            Console.WriteLine("------------------------\n");
 
-    // Ask the user to choose an option.
-    Console.WriteLine("Choose an option from the following list:");
-    Console.WriteLine("\t1 - DisplayInventory");
-    Console.WriteLine("\t2 - DisplayBalance");
-    Console.WriteLine("\t3 - UpdateInventory");
-    Console.WriteLine("\t4 - SellItem");
-    Console.Write("Your option? ");
+            // Ask the user to choose an option.
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\t1 - DisplayInventory");
+            Console.WriteLine("\t2 - DisplayBalance");
+            Console.WriteLine("\t3 - UpdateInventory");
+            Console.WriteLine("\t4 - SellItem");
+            Console.Write("Your option? ");
 
-    // Use a switch statement to do the math.
-    switch (Console.ReadLine())
-    {
-        case "1":
-                   var inventory =  fileItemsRepository.GetInventory();
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Liste des articles : ");
+
+                    var inventory =  fileItemsRepository.GetInventory();
                     foreach(var item in inventory)
                     {
-                        Console.WriteLine("Name",item.name);
-                        Console.WriteLine("SellIn",item.sellIn);
-                        Console.WriteLine("Quality",item.quality);
+                        Console.WriteLine("- Nom : " + item.name + " \t/ Sell in : " + item.sellIn + " \t/ Qualité : " + item.quality);
                     }
                     break;
-        case "2":
-            break;
-        case "3":
-            break;
-        case "4":
-            break;
-    }
-    // Wait for the user to respond before closing.
-    Console.Write("Press any key to close the Calculator console app...");
-    Console.ReadKey();
-    }
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+            }
 }
 }
