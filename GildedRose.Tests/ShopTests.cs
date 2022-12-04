fc594db0bf93255431d597edafdf08a303825ab2
adrienDespretz <adriendespretz40@gmail.com>
2022-12-04 01:17:07 +0100

    add test for SaveInventory

--- GildedRose.Tests/ShopTests.cs
+++ GildedRose.Tests/ShopTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using GildedRose.Appli;
 using GildedRose.Items;
+using GildedRose.ItemsRepository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GildedRose.Tests
@@ -8,69 +9,77 @@ namespace GildedRose.Tests
     [TestClass]
     public class ShopTests
     {
-        public Shop shop;
-        public ItemsGateway repository;
+        public ShopInteractor shop;
+        public ItemsGateway itemGateway;
         [TestInitialize]
         public void Setup(){
-            this.shop = new Shop(repository);
-            this.shop.UpdateQuality();
+            this.shop = new ShopInteractor();
+            this.shop.UpdateInventory();
+
+            this.itemGateway = new InMemoryItemsRepository();
         }
 
         [TestMethod]
         public void Should_UpdateItemProperties(){
-            Assert.AreEqual(9, this.repository.GetInventory()[0].sellIn);
-            Assert.AreEqual(7, this.repository.GetInventory()[0].quality);
+            Assert.AreEqual(9, this.shop.GetInventory()[0].sellIn);
+            Assert.AreEqual(7, this.shop.GetInventory()[0].quality);
         }
 
         [TestMethod]
         public void Should_DecreaseQualityTwiceAsFastAfterExpiration(){
-            Assert.AreEqual(6, this.repository.GetInventory()[1].quality);
+            Assert.AreEqual(6, this.shop.GetInventory()[1].quality);
         }
 
         [TestMethod]
         public void Should_NotHaveNegativeQuality(){
-            Assert.AreEqual(0, this.repository.GetInventory()[2].quality);
+            Assert.AreEqual(0, this.shop.GetInventory()[2].quality);
         }
 
         [TestMethod]
         public void Should_IncreaseAgedBrieQuality(){
-            Assert.AreEqual(5, this.repository.GetInventory()[3].quality);
+            Assert.AreEqual(5, this.shop.GetInventory()[3].quality);
         }
 
         [TestMethod]
         public void Should_NotIncreaseQualityOverFifty(){
-            Assert.AreEqual(50, this.repository.GetInventory()[4].quality);
+            Assert.AreEqual(50, this.shop.GetInventory()[4].quality);
         }
 
         [TestMethod]
         public void Should_NotUpdateLegendaryItemProperties(){
-            Assert.AreEqual(5, this.repository.GetInventory()[5].sellIn);
-            Assert.AreEqual(80, this.repository.GetInventory()[5].quality);
+            Assert.AreEqual(5, this.shop.GetInventory()[5].sellIn);
+            Assert.AreEqual(80, this.shop.GetInventory()[5].quality);
         }
 
         [TestMethod]
         public void Should_IncreaseBackstagePassQuality(){
-            Assert.AreEqual(11, this.repository.GetInventory()[6].quality);
+            Assert.AreEqual(11, this.shop.GetInventory()[6].quality);
         }
 
         [TestMethod]
         public void Should_IncreaseBackstagePassQualityByTwoTenDaysBefore(){
-            Assert.AreEqual(12, this.repository.GetInventory()[7].quality);
+            Assert.AreEqual(12, this.shop.GetInventory()[7].quality);
         }
 
         [TestMethod]
         public void Should_IncreaseBackstagePassQualityByThreeFiveDaysBefore(){
-            Assert.AreEqual(13, this.repository.GetInventory()[8].quality);
+            Assert.AreEqual(13, this.shop.GetInventory()[8].quality);
         }
 
         [TestMethod]
         public void Should_SetBackstagePassQualityToZeroAfterEvent(){
-            Assert.AreEqual(0, this.repository.GetInventory()[9].quality);
+            Assert.AreEqual(0, this.shop.GetInventory()[9].quality);
         }
 
         [TestMethod]
         public void Should_DecreaseConjuredItemsTwiceAsFastAsGenericItems(){
-            Assert.AreEqual(6, this.repository.GetInventory()[10].quality);
+            Assert.AreEqual(6, this.shop.GetInventory()[10].quality);
+        }
+
+        [TestMethod]
+        public void Should_SaveInventory(){
+            itemGateway.SaveInventory(this.shop.GetInventory());
+            CollectionAssert.AreEqual(this.shop.GetInventory(), itemGateway.GetInventory());
         }
 
     }
